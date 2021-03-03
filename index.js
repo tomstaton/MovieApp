5e248802dc30bdef2c0d647257de26e19d1fb4ff
tomstaton <74940768+tomstaton@users.noreply.github.com>
2021-03-03 17:08:41 -0500

    added expres and morgan

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+
+let topMovies = [
+  {
+    title: 'Zodiac',
+  },
+  {
+    title: 'Superbad'
+  },
+  {
+    title: 'The Thing'
+  },
+  {
+    title: 'Rosemary\'s baby'
+  },
+  {
+    title: 'After Hours'
+  },
+  {
+    title: 'Once Upon a Time... In Hollywood'
+  },
+  {
+    title: 'The Apartment'
+  },
+  {
+    title: 'Inside Llewyn Davis'
+  },
+  {
+    title: 'A Star is Born'
+  },
+  {
+    title: 'The Evil Dead 2'
+  }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('My top movies');
+});
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies)
+});
+
+app.listen(8080, () => {
+  console.log('app is running');
+});
