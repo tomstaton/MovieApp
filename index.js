5fbf6f08929e3f19432d7a27bea435e5f9852c44
tomstaton <74940768+tomstaton@users.noreply.github.com>
2021-04-30 20:52:58 -0400

    heroku e10

--- index.js
+++ index.js
@@ -343,7 +343,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', {session: false}), (
     });
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port, '0.0.0.0',() => {
   console.log('Listening on Port ' + port);
 });
