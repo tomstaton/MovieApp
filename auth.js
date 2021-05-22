c988f9f0239ca3ed2ce0e99d36d71c8b8b205479
tomstaton <74940768+tomstaton@users.noreply.github.com>
2021-05-22 14:58:24 -0400

    cors use added back

--- auth.js
+++ auth.js
@@ -1,26 +1,26 @@
 //log in
-const jwtSecret = 'your_jwt_secret';
+const jwtSecret = "your_jwt_secret";
 
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
+const jwt = require("jsonwebtoken"),
+  passport = require("passport");
 
-require('./passport');
+require("./passport");
 
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username,
-    expiresIn: '7d',
-    algorithm: 'HS256'
+    expiresIn: "7d",
+    algorithm: "HS256",
   });
-}
+};
 
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
+  router.post("/login", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: 'Something is not right',
-          user: user
+          message: "Something is not right",
+          user: user,
         });
       }
       req.login(user, { session: false }, (error) => {
@@ -28,8 +28,8 @@ module.exports = (router) => {
           res.send(error);
         }
         let token = generateJWTToken(user.toJSON());
-        return res.json({user, token});
+        return res.json({ user, token });
       });
     })(req, res);
   });
-}
+};
