07e47d4e3a10886805b47f2ccb4d9d92a6ad4732
tomstaton <74940768+tomstaton@users.noreply.github.com>
2021-05-22 23:33:24 -0400

    original cors imp

--- index.js
+++ index.js
@@ -17,8 +17,25 @@ app.use(morgan("common"));
 const auth = require("./auth")(app);
 
 const cors = require("cors");
-app.use(cors());
-app.options("*", cors());
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://localhost:1234",
+  "https://internetbasedmoviedata.herokuapp.com",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS policy for this application doesn't allow access from origin" +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 const Movies = Models.Movie;
 const Users = Models.User;
